0a8cf6a371cbb7c6290831f42b0aaaa8dce949d6
Aastha <aasthashar@gmail.com>
2022-12-13 19:37:46 +0100

    Day 08 part 1 Not a very nice solution imo

--- 08/08.py
+++ 08/08.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-with open('test-input.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     elvish_input = f.read().split('\n')
 
 elvish_matrix = np.array([np.array(list(row), dtype=int) for row in elvish_input])
@@ -10,11 +10,18 @@ visible_trees = (len(elvish_matrix) -2) * 2 + len(elvish_matrix[0]) + len(elvish
 for i in range(1, len(elvish_matrix)-1):
     for j in range(1, len(elvish_matrix[0])-1):
         tree_height = elvish_matrix[i][j]
-        # print(elvish_matrix[i, 0:j+1]) # check row from beginning until tree
-        # print(elvish_matrix[i, j:])
-        # print(elvish_matrix[0:i+1, j])
-        # print(elvish_matrix[i:, j])
-        if elvish_matrix[i, 0:j+1].max() == tree_height or elvish_matrix[i, j:].max() == tree_height or elvish_matrix[0:i+1, j].max() == tree_height or elvish_matrix[i:, j].max() == tree_height:
+        row_until = elvish_matrix[i, 0:j+1]
+        row_after = elvish_matrix[i, j:]
+        col_until = elvish_matrix[0:i+1, j]
+        col_after = elvish_matrix[i:, j]
+        if np.count_nonzero(row_until == tree_height) == 1 and row_until.max() == tree_height:
             visible_trees += 1
+        elif np.count_nonzero(row_after == tree_height) == 1 and row_after.max() == tree_height:
+            visible_trees += 1
+        elif np.count_nonzero(col_until == tree_height) == 1 and col_until.max() == tree_height:
+            visible_trees += 1
+        elif np.count_nonzero(col_after == tree_height) == 1 and col_after.max() == tree_height:
+            visible_trees +=1 
+            
 
-print(visible_trees)
+print("Number of trees visible:", visible_trees)
