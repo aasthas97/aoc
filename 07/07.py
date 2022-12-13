a049d436a0547794fb8dec148a89b27bb4d38157
Aastha Sharma <asharma@ant-neuro.com>
2022-12-13 19:19:11 +0100

    08 progress

--- 07/07.py
+++ 07/07.py
@@ -2,3 +2,4 @@ with open('test-input.txt', 'r') as f:
     elvish_input = f.read().split('\n')
 
 print(elvish_input)
+# Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
--- 08/08.py
+++ 08/08.py
@@ -1,2 +1,20 @@
-with open('input.txt', 'r') as f:
-    elvish_input = f.read()
+import numpy as np
+
+with open('test-input.txt', 'r') as f:
+    elvish_input = f.read().split('\n')
+
+elvish_matrix = np.array([np.array(list(row), dtype=int) for row in elvish_input])
+# count trees visible at the edges
+visible_trees = (len(elvish_matrix) -2) * 2 + len(elvish_matrix[0]) + len(elvish_matrix[-1])
+
+for i in range(1, len(elvish_matrix)-1):
+    for j in range(1, len(elvish_matrix[0])-1):
+        tree_height = elvish_matrix[i][j]
+        # print(elvish_matrix[i, 0:j+1]) # check row from beginning until tree
+        # print(elvish_matrix[i, j:])
+        # print(elvish_matrix[0:i+1, j])
+        # print(elvish_matrix[i:, j])
+        if elvish_matrix[i, 0:j+1].max() == tree_height or elvish_matrix[i, j:].max() == tree_height or elvish_matrix[0:i+1, j].max() == tree_height or elvish_matrix[i:, j].max() == tree_height:
+            visible_trees += 1
+
+print(visible_trees)
